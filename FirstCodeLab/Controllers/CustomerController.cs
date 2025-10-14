c78ff2420667db7288d819d062e224eca70bfef9
Rely Ky <relyky@hotmail.com>
2025-10-14 11:03:54 +0800

    應用測試

---
+++ FirstCodeLab/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using FirstCodeLab.DatabaseContext;
+using FirstCodeLab.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstCodeLab.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CustomerController(
+  AppDbContext _dbctx
+  ) : ControllerBase
+{
+  [HttpPost("[action]")]
+  public IEnumerable<Customer> Search()
+  {
+    return _dbctx.Customer.Select(c => c);
+  }
+
+  [HttpPost("[action]")]
+  public Customer Create(Customer customer) 
+  {
+    using var txn = _dbctx.Database.BeginTransaction();
+    _dbctx.Customer.Add(customer);
+    _dbctx.SaveChanges();
+    txn.Commit();
+    return customer;
+  }
+}
